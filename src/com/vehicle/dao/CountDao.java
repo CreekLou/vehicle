a397e3bf6d2750beeb5fa20ee42465c7d9c7cb4f
CreekLou <lgcssx@gmail.com>
2014-03-19 09:59:51 +0800

    add 页面访问次数

---
+++ src/com/vehicle/dao/CountDao.java
@@ -0,0 +1,22 @@
+package com.vehicle.dao;
+
+import org.springframework.stereotype.Repository;
+
+import com.vehicle.domain.Count;
+
+@Repository
+public class CountDao extends BaseDao<Count> {
+	/*
+	 * protected final String GET_COUNT_NUM =
+	 * "select countNum from Count where countId=?"; protected final String
+	 * UPDATE_COUNT_NUM =
+	 * "update Count count set count.countNum=? where count.countId=?";
+	 * 
+	 * // 获取CountNum数目 public long getCountNum() { Iterator iter =
+	 * getHibernateTemplate().iterate(GET_COUNT_NUM); return ((Long)
+	 * iter.next()); }
+	 * 
+	 * // 修改CountNum数目 public void updateCountNum() {
+	 * getHibernateTemplate().update(UPDATE_COUNT_NUM); }
+	 */
+}
